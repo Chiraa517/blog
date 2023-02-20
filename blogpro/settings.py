fdd163978814a900d01fe360f10a6d22a6827faa
Chiraa517 <chiraastudio@gmail.com>
2023-02-20 19:56:12 +0500

    Fixed untracked files

--- blogpro/settings.py
+++ blogpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e6!f7(de1d-%6ajwx=98=t-$(o^omph_m_ae22u9=l=fl!knx&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
